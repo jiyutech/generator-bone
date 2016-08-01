ccf714a1c994334e9420f57f18b9d27962612316
Alexius Lee <alexorz@jiyu.tech>
2016-08-01 08:51:19 +0800

    v1.2.2 更新点： * 更新bone内置 navigator 中间件，修复其在解析URL参数时的问题

--- generators/.bone/src/site/middleware/navigator.js
+++ generators/.bone/src/site/middleware/navigator.js
@@ -3,14 +3,27 @@
 const Router = require('koa-router');
 const _ = require('lodash');
 
-module.exports = function( navigatorData, options ){
-
-  options = _.assign({
-    'parseUrl': false // 是否使用params来解析URL中的``:foo`变量
-  }, options || {});
+module.exports = function( navigatorData ){
 
   var urlIndexMap = {};
   var urlCateObjCrumbMap = {};
+
+  function parseUrlParams( url, srcParams ){
+    // var paramReg = /\:[^/]+/g;
+    // var params = _.extend({}, srcParams);
+    // (url.match(paramReg) || []).forEach(function( matchRes ){
+    //   var key = matchRes.slice(1);
+    //   if ( params[key] === undefined ) params[key] = '';
+    // });
+    var parsedUrl;
+    try {
+      parsedUrl = Router.url( url, srcParams );
+    }
+    catch(e) {
+      parsedUrl = url;
+    }
+    return parsedUrl;
+  }
 
   (function(){
     var geneMap = function(cate, index, indexStack, cateObjStack ){
@@ -50,17 +63,17 @@ module.exports = function( navigatorData, options ){
         currentNavCate,
         currentNavCrumb,
         matchLength = 0,
-        outputCates = options.parseUrl ? JSON.parse(JSON.stringify(navigatorData.cates)) : navigatorData.cates;
+        outputCates = JSON.parse(JSON.stringify(navigatorData.cates));
 
     for( var srcUrl in urlIndexMap ) {
       // /:param/ -> /val/
-      var parsedUrl = Router.url( srcUrl, this.params );
+      var parsedUrl = parseUrlParams( srcUrl, this.params );
       if ( currPath == parsedUrl.replace(/\?.*/,'') && currUrl.indexOf( parsedUrl ) === 0 ) {
         // 保留匹配度更高的条目，例如：当前url是`a?a=1&b=2`，在`/a`和`/a?a=1`中优先匹配后者。
         if ( parsedUrl.length > matchLength ) {
           matchLength = parsedUrl.length;
-          currentNavIndex = options.parseUrl ? JSON.parse(JSON.stringify( urlIndexMap[ srcUrl ] )) : urlIndexMap[ srcUrl ];
-          currentNavCrumb = options.parseUrl ? JSON.parse(JSON.stringify( urlCateObjCrumbMap[ srcUrl ] )) : urlCateObjCrumbMap[ srcUrl ];
+          currentNavIndex = JSON.parse(JSON.stringify( urlIndexMap[ srcUrl ] ));
+          currentNavCrumb = JSON.parse(JSON.stringify( urlCateObjCrumbMap[ srcUrl ] ));
           currentNavCate = currentNavCrumb ? currentNavCrumb.slice(-1)[0] : null;
         }
       }
@@ -68,11 +81,9 @@ module.exports = function( navigatorData, options ){
 
     // 解析输出数据中的URL
     // /:param/ -> /val/
-    if ( options.parseUrl ) {
-      handleCates(outputCates.concat(currentNavCrumb).concat([currentNavCate]), function(cate){
-        if ( cate.url ) cate.url = Router.url( cate.url, this.params );
-      }.bind(this));
-    }
+    handleCates(outputCates.concat(currentNavCrumb).concat([currentNavCate]), function(cate){
+      if ( cate.url ) cate.url = parseUrlParams( cate.url, this.params );
+    }.bind(this));
 
     this.renderMixin.navigator = {
       currentNavIndex : currentNavIndex,
